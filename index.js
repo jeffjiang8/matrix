57f3cf30f1e80a22b7229e3b70e153df86b97862
Jeff Jiang <jeffjiang8@gmail.com>
2020-05-06 19:37:11 -0400

    setup

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function matrix(n) {
+    const results = []
+
+    for (let i = 0; i < n; i++) {
+        results.push([])
+    }
+
+    let counter = 1
+    let startColumn = 0
+    let startRow = 0
+    let endColumn = n - 1
+    let endRow = n - 1
+}

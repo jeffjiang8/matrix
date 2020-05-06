5e2ccb8b65e9186f439a26beb93739a352f0c566
Jeff Jiang <jeffjiang8@gmail.com>
2020-05-06 19:37:43 -0400

    done

--- index.js
+++ index.js
@@ -11,4 +11,35 @@ function matrix(n) {
     let startRow = 0
     let endColumn = n - 1
     let endRow = n - 1
+
+    while (startColumn <= endColumn && startRow <= endRow) {
+        // top row
+        for (let i = startColumn; i <= endColumn; i++) {
+            results[startRow][i] = counter
+            counter++
+        }
+        startRow++
+
+        // right column
+        for (let i = startRow; i <= endRow; i++) {
+            results[i][endColumn] = counter
+            counter++
+        }
+        endColumn--
+
+        // bottom row
+        for (let i = endColumn; i >= startColumn; i--) {
+            results[endRow][i] = counter
+            counter++
+        }
+        endRow--
+
+        // left column
+        for (let i = endRow; i >= startRow; i--) {
+            results[i][startColumn] = counter
+            counter++
+        }
+        startColumn++
+    }
+    return results
 }
